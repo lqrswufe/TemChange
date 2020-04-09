1aa983092c979261d3c9d4f48e3009460c0c893c
lqrswufe <1096026323@qq.com>
2020-04-09 16:36:41 +0800

    score2 Homwork 2team

--- app/src/main/java/com/swufe/firstapp/SencondActivity.java
+++ app/src/main/java/com/swufe/firstapp/SencondActivity.java
@@ -10,32 +10,57 @@ import android.widget.TextView;
 
 public class SencondActivity extends AppCompatActivity {
      TextView score;
-    TextView textName;
+    TextView score2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sencond);
         score= (TextView) findViewById(R.id.score);
-
+        score2= (TextView) findViewById(R.id.score2);
     }
     public void btnAdd1(View btn){
-        showScore(1);
+        if(btn.getId()==R.id.btn_1){
+            showScore(1);
+
+    }else {
+            showScore2(1);
+        }
     }
     public void btnAdd2(View btn){
-        showScore(2);
+        if(btn.getId()==R.id.btn_2){
+            showScore(2);
+
+        }else {
+            showScore2(2);
+
+        }
+
     }
     public void btnAdd3(View btn){
-        showScore(3);
+        if(btn.getId()==R.id.btn_3){
+            showScore(3);
+
+        }else {
+            showScore2(3);
+        }
     }
     public void btnResert(View btn){
         score.setText("0");
-
+        score2.setText("0");
     }
     private void showScore(int inc){
         Log.i("show","inc=" + inc);
         String oldScore = (String) score.getText();
         int newScore = Integer.parseInt(oldScore)+inc;
         score.setText("" + newScore );
+
+    }
+    private void showScore2(int inc){
+        Log.i("show","inc=" + inc);
+        String oldScore = (String) score2.getText();
+        int newScore = Integer.parseInt(oldScore)+inc;
+        score2.setText("" + newScore );
 
     }
 }
